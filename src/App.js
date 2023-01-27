95569183410fc27a562e0c16ba003a57260d2d93
Jaxz10 <42484410+Jaxz102@users.noreply.github.com>
2023-01-27 12:49:17 -0500

    add figure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
-import Home from './components/home/Home.jsx';
-import Figures from './components/figures/Figures.jsx';
-import WhySignUp from './components/whysignup/Whysignup.jsx';
-import Timeline from './components/timeline/Timeline.jsx';
-import Partners from './components/partners/Partners.jsx';
+import Home from './components/Home/Home.jsx';
+import Figures from './components/Figures/Figures.jsx';
+import WhySignUp from './components/Whysignup/Whysignup.jsx';
+import Timeline from './components/Timeline/Timeline.jsx';
+import Partners from './components/Partners/Partners.jsx';
 
 function App() {
   return (
