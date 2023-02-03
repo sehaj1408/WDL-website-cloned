d79bab94cc8568240271ac21190cd2f90bb2da10
Tiago <talves4@uwo.ca>
2023-02-02 19:23:52 -0500

    implemented partners
    added logos to images folder, formatted text

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -2,8 +2,36 @@ import './partners.css';
 
 const Partners = () => {
     return (
-        <></>
+        <div class="partnersHeader" id="f">
+
+            <div>
+            <h1 id="partnersh1">Over X companies are partnered for WDL</h1>
+            <h4 id="partnersh4">Cupcake danish tiramisu powder soda ginger cake.</h4>
+            <h4 id="partnersh4">Dessert carrot cake tartsweet marzipan for fruitcake.</h4>
+            </div>
+
+            <div class="container2">
+                <div class="figure--block2" >
+                    <img src="/images/partnerslogo1.png" alt="partner1"></img>
+                </div>
+                <div class="figure--block2">
+                    <img src="/images/partnerslogo2.png" alt="partner2"></img>
+                </div>
+                <div class="figure--block2">
+                    <img src="/images/partnerslogo3.png" alt="partner3"></img>
+                </div>
+                <div class="figure--block2">
+                    <img src="/images/partnerslogo4.png" alt="partner4"></img>
+                </div>
+            </div>
+
+            <div>
+                <h6 id="bottomTag">Made by Western Developers Society</h6>
+            </div>
+
+        </div>
     );
 };
+
 
 export default Partners
